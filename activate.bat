a38bdf76c81d471d67cc66c2088250835694a1c5
Randy420Marsh <46230578+Randy420Marsh@users.noreply.github.com>
2025-01-07 08:13:36 +0200

    update

--- activate.bat
+++ activate.bat
@@ -6,4 +6,9 @@ cd %current_path%
 
 setlocal enabledelayedexpansion
 
+REM pip install -r requirements.txt
+
+REM python.exe -m pip install --upgrade pip
+
 IF exist ./venv (cmd /k call .\venv\scripts\activate.bat)  ELSE (cmd /k python -m venv venv && cmd /k call .\venv\scripts\activate.bat)
+
